f68cf957e1ac0cc56b09d291efa17defe93a52de
bug-author <iabdullahbintahir@gmail.com>
2024-07-28 01:34:45 +0500

    chore: cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import RainbowkitProvider from "./providers/RainbowkitProvider.tsx";
 import Layout from "./components/Layout.tsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,15 +9,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RainbowkitProvider>
-      <ToastContainer />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/:filename" element={<App />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </RainbowkitProvider>
+    <ToastContainer />
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/:filename" element={<App />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
